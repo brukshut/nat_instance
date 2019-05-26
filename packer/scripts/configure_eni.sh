84f9dabe253385bb9cf8fd32c30f1249e040a143
Christian Gough <brukshut@gmail.com>
2019-05-26 13:06:21 -0400

    Fetch eni_ctl.sh, along with support scripts and files, from remote git repository.

--- packer/scripts/configure_eni.sh
+++ packer/scripts/configure_eni.sh
@@ -1,50 +1,45 @@
-#!/bin/bash 
+#!/bin/bash -x
 
 ##
 ## configure_eni.sh
+## wrapper script to fetch eni_ctl.sh and support files from git
 ##
-PATH=/bin:/sbin:/usr/bin:/usr/sbin
 
 ## functions
-function die { echo "$*" 1>&2 && exit 1; }
-
-function add_script {
-  local script=$1
-  [ -e /tmp/${script} ] && 
-    ( sudo mv /tmp/${script} /usr/local/sbin/${script}
-      sudo chown root:root /usr/local/sbin/${script}
-      sudo chmod 755 /usr/local/sbin/${script} ) ||
-      die "file not found: /tmp/${script}"
+function fetch_eni_ctl {
+  cleanup
+  git clone https://github.com/brukshut/eni_ctl /tmp/eni_ctl
 }
 
-function add_file {
-  local file=$1
-  local directory=$2
-  [[ -d ${directory} ]] || die "${directory} does not exist."
-  [[ -e /tmp/${file} ]] && 
-    ( sudo mv /tmp/${file} ${directory}
-      sudo chmod 644 ${directory}/${file}
-      sudo chown root:root ${directory}/${file} ) ||
-      die "no such file: /tmp/${file}"
+function cleanup {
+  [[ -d /tmp/eni_ctl ]] && sudo rm -rf /tmp/eni_ctl
 }
 
-function add_service {
-  local service=$1
-  [[ -e /tmp/${service} ]] &&
-    ( sudo mv /tmp/${service} /lib/systemd/system/${service} &&
-      sudo chown root:root /lib/systemd/system/${service} ) ||
-      die "no such file: /tmp/${service}"
+function copy_files {
+  for file in dhclient.conf interfaces eni.service; do
+    [[ -e /tmp/eni_ctl/files/${file} ]] &&
+      sudo cp /tmp/eni_ctl/files/${file} /tmp
+  done
+  for script in eni_ctl.sh add_routes.sh configure_eni.sh configure_interfaces.sh; do 
+    [[ -e /tmp/eni_ctl/scripts/${script} ]] && 
+      sudo cp /tmp/eni_ctl/scripts/${script} /tmp
+  done
 }
-    
-function reload_service {
-  local service=$1
-  sudo systemctl daemon-reload && sudo systemctl enable ${service} ||
-    die
+
+function configure_eni {
+  fetch_eni_ctl && copy_files
+  for script in configure_eni.sh configure_interfaces.sh; do
+    [[ -e /tmp/${script} ]] && 
+      ( sudo chmod +x /tmp/${script}
+        echo "sudo /tmp/${script}" )
+  done
+  cleanup
 }
+
 ## end functions
 
 ## main
-add_script eni_ctl.sh 
-add_service eni.service 
-reload_service eni.service
+
+configure_eni
+
 ## end main
